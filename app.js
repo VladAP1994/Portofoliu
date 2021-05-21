7de4b6615b5f415c9bdd2bd5139624a13fcdc0ed
Vlad Alexandru Popa <vladap1994@gmail.com>
2021-05-21 03:46:13 +0300

    fin.

---
+++ app.js
@@ -0,0 +1,21 @@
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display == "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+// var myLinks = document.getElementById("myLinks");
+// myLinks.style.maxHeight = "0px";
+
+// function myFunction(){
+//   if(myLinks.style.maxHeight == "0px"){
+//     myLinks.style.maxHeight = "130px";
+//   }else{
+//     myLinks.style.maxHeight = "0px";
+//   }
+// }
+// myFunction();
